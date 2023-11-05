9da3efe3cf685189a23cf74f2952a7788e204fd5
parker-martel <parkermartel48@gmail.com>
2023-11-05 13:06:06 -0800

    Started on website portion

--- flowbite-flask/app.py
+++ flowbite-flask/app.py
@@ -1,11 +1,56 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory, url_for
+from flask_uploads import IMAGES, UploadSet, configure_uploads
+import os
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from wtforms import SubmitField
+from imagedetection import *
+from PIL import Image
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.urandom(24)
+app.config["UPLOADED_PHOTOS_DEST"] = "uploads"
 
-@app.route("/")
-@app.route("/index")
-def index():
-	return render_template("index.html")
+photos = UploadSet("photos", IMAGES)
+configure_uploads(app, photos)
+
+class UploadPhoto(FlaskForm):
+	photo = FileField(
+		validators=[
+			FileAllowed(photos, 'Only images allowed'),
+			FileRequired('File field should not be empty')
+		]
+	)
+	submit = SubmitField('Upload')
+
+@app.route('/uploads/<filename>')
+def get_file(filename):
+	return send_from_directory(app.config["UPLOADED_PHOTOS_DEST"], filename)
+
+
+@app.route("/", methods=['GET', 'POST'])
+def upload_image():
+	form = UploadPhoto()
+	snake_name = None
+	if form.validate_on_submit():
+		filename = photos.save(form.photo.data)
+		file_url = url_for('get_file', filename=filename)
+		print(filename)
+		print(file_url)
+		print(os.getcwd())
+		loader = f"{os.getcwd()}{file_url}"
+		print(loader)
+		snake_name = image_recognition(loader)
+	else:
+		file_url = None
+	if snake_name is not None:
+		return render_template('index.html', form=form, file_url=file_url, snake_name=snake_name)
+	else:
+		return render_template('index.html', form=form, file_url=file_url)
+
+
+
 
 if __name__ == '__main__':
 	app.run(debug=True, port=5001)
+
---
+++ flowbite-flask/imagedetection.py
@@ -0,0 +1,23 @@
+# run this file and look at output in terminal. We need to find a way
+# to determine if the snake that is detected is venomous or not
+# add add all these files to the website
+from imageai.Classification import ImageClassification
+import os
+from torchvision.models import resnet50, ResNet50_Weights
+
+execution_path = os.getcwd()
+
+# Using the resnet-50 model. ImageAI has 3 other models in the documentation we could test aswell. 
+prediction = ImageClassification() # create an instance of the ImageClassification class from the ImageAI library
+prediction.setModelTypeAsResNet50() # set the model type that the ImageClassification object will use to ResNet-50
+prediction.setModelPath(os.path.join(execution_path, "resnet50-19c8e357.pth")) # set the path to the model weights file that the ImageClassification object will use
+prediction.loadModel() # load the model with the specified model type (ResNet-50) and model weights (from the included .pth file)
+
+def image_recognition(image):
+    print(image)
+    predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, image), result_count=5) # call the classifyImage method of the ImageClassification object to classify the snake in the image
+    for eachPrediction, eachProbability in zip(predictions, probabilities): # Iterates through the top 5 predictions in order of decreasing probability
+        print(eachPrediction , " : " , eachProbability)
+    predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, image), result_count=1)
+    print(f"\n\nThis snake is: {predictions[0]}\n\n")
+    return predictions[0]
