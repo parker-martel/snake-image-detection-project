dc4059657d8a79d32310e1fbc0b53b0b2a671fc2
parker-martel <parkermartel48@gmail.com>
2023-11-06 18:49:36 -0800

    Old images get deleted

--- flowbite-flask/app.py
+++ flowbite-flask/app.py
@@ -5,7 +5,6 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import SubmitField
 from imagedetection import *
-from PIL import Image
 
 # Initializa Flask App
 app = Flask(__name__)
@@ -14,6 +13,7 @@ app.config["UPLOADED_PHOTOS_DEST"] = "uploads"
 
 photos = UploadSet("photos", IMAGES)
 configure_uploads(app, photos)
+prev_image = None
 
 # Flask form to upload image
 class UploadPhoto(FlaskForm):
@@ -34,14 +34,19 @@ def get_file(filename):
 def upload_image():
 	form = UploadPhoto()
 	snake_name = None
+
 	if form.validate_on_submit(): # Check if image was uploaded
+		global prev_image
+		if prev_image: # Delete the previous uploaded image
+			os.remove(prev_image)
 		filename = photos.save(form.photo.data)
 		file_url = url_for('get_file', filename=filename)
-		print(filename)
-		print(file_url)
-		print(os.getcwd())
+		dir = f"{os.getcwd()}/uploads"
+		# if len(os.listdir(f'{dir}')) > 2:
+		# 	pass
+		print("\n\nLOADER: ", f"{os.getcwd()}{file_url}")
 		loader = f"{os.getcwd()}{file_url}" # Add correct path to image
-		print(loader)
+		prev_image = loader
 		snake_name = image_recognition(loader) # Run image recognition function in imagedetection.py
 	else:
 		file_url = None
