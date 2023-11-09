c9c018afe96a1202385f6f49ca8b868fc18192ed
parker-martel <parkermartel48@gmail.com>
2023-11-08 16:35:12 -0800

    added custom model

--- flowbite-flask/imagedetection.py
+++ flowbite-flask/imagedetection.py
@@ -1,7 +1,7 @@
 # run this file and look at output in terminal. We need to find a way
 # to determine if the snake that is detected is venomous or not
 # add add all these files to the website
-from imageai.Classification import ImageClassification
+from imageai.Classification.Custom import CustomImageClassification
 import os
 from torchvision.models import resnet50, ResNet50_Weights
 
@@ -10,7 +10,11 @@ execution_path = os.getcwd()
 # Using the resnet-50 model
 prediction = ImageClassification() # create an instance of the ImageClassification class from the ImageAI library
 prediction.setModelTypeAsResNet50() # set the model type that the ImageClassification object will use to ResNet-50
-prediction.setModelPath(os.path.join(execution_path, "resnet50-19c8e357.pth")) # set the path to the model weights file that the ImageClassification object will use
+#prediction.setModelPath(os.path.join(execution_path, "resnet50-19c8e357.pth")) # set the path to the model weights file that the ImageClassification object will use
+
+prediction.setModelPath(os.path.join(execution_path, "resnet50-snakes-test_acc_0.23901_epoch-48.pt"))
+prediction.setJsonPath(os.path.join(execution_path, "snakes_model_classes.json"))
+
 prediction.loadModel() # load the model with the specified model type (ResNet-50) and model weights (from the included .pth file)
 
 def image_recognition(image):
